cc4efdb2e750275971845d3ecfbe4015cbe3f930
mo <mo>
2026-03-24 17:02:52 +0800

    for

--- code/var.py
+++ code/var.py
@@ -6,3 +6,25 @@ print(i)
 s = '''This is a multi-line string.
 This is the second line.'''
 print(s)
+
+print("=====================")
+
+attributes = ['name', 'dob', 'gender']
+values = [['jason', '2000-01-01', 'male'],
+          ['mike', '1999-01-01', 'male'],
+          ['nancy', '2001-02-01', 'female']
+          ]
+
+# 实现了功能了，可能不够 pythonic
+data_list = []
+for valueList in values:
+    data = {}
+    for i in range(0, len(attributes)):
+        data[attributes[i]] = valueList[i]
+    data_list.append(data)
+print(data_list)
+
+# expected output:
+# [{'name': 'jason', 'dob': '2000-01-01', 'gender': 'male'},
+#  {'name': 'mike', 'dob': '1999-01-01', 'gender': 'male'},
+#  {'name': 'nancy', 'dob': '2001-02-01', 'gender': 'female'}]
